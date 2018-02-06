c644a515f95083cababef007dd3516ffdf6a8799
Christopher Leveston <christopherleveston@gmail.com>
2018-02-06 00:14:13 -0600

    We got our own domain name

--- constructionMsg.js
+++ constructionMsg.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 /*Under Contstruction Message*/
 var constructionPage;
-constructionAlert = "Under Construction. Microsoft Edge Bug FIXED!";
+constructionAlert = "Under Construction. Microsoft Edge Bug FIXED!. We got our own domain name!";
 
 alert(constructionAlert);
