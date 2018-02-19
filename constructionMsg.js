e4062727a5e9ab23303208690e7466be03e3be87
Christopher Leveston <christopherleveston@gmail.com>
2018-02-18 19:15:24 -0600

    Added to Published Work and extra script.
    Added more photos in Published Work page, and also added an extra script in the only javascript file to take out the coming soon in the button.

--- constructionMsg.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-/*Under Contstruction Message*/
-var constructionPage;
-constructionAlert = "Under Construction. Microsoft Edge Bug FIXED!. We got our own domain name!";
-
-alert(constructionAlert);
--- copyrightYear.js
+++ copyrightYear.js
@@ -5,3 +5,8 @@ var year = yearDate.getFullYear();
 
 var el = document.getElementById("yearDate");
 el.textContent = year;
+
+//Script to change published works out of coming Soon
+var renamePublished = document.getElementById("PublishedWorksButton");
+var elRename = "Published Work";
+renamePublished.textContent = elRename;
