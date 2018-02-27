01bd20d75f4ec2a322b6d4efa93d148bc54bd620
Christopher Leveston <christopherleveston@gmail.com>
2018-02-26 19:16:56 -0600

    Update 1.1(3)
    created script to print coming soon. The "coming soon" string can now be used anywhere in the javascript and website.

--- theSite.js
+++ theSite.js
@@ -3,3 +3,14 @@ var chicagoBubbleImage = document.getElementById('bubblesImage');
 $('document').ready(function() {
     $(chicagoBubbleImage).hide().fadeIn(2500);
 });
+
+//Creating the coming soon element and object
+var comingSoonEl = document.createElement('SPAN');
+var comingSoonTextEl = document.createTextNode('(coming soon)');
+
+comingSoonEl.appendChild(comingSoonTextEl);
+
+var comingSoonString = document.getElementsByTagName('li')[3];
+$('document').ready(function() {
+    $(comingSoonString).after(comingSoonEl);
+});
