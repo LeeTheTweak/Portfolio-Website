e5c990def7791cf5c5601f628a88e91a916dc1f4
Christopher Leveston <christopherleveston@gmail.com>
2018-07-11 20:52:23 -0500

    More JS and Web Projects
    Added more web projects in the home page. Also, I added more javascript for those elements.

--- theSite.js
+++ theSite.js
@@ -6,7 +6,7 @@ $('document').ready(function() {
 
 //Creating the coming soon element and object
 var comingSoonEl = document.createElement('SPAN');
-var comingSoonTextEl = document.createTextNode('(coming soon)');
+var comingSoonTextEl = document.createTextNode('(Look Below)');
 
 comingSoonEl.appendChild(comingSoonTextEl);
 
@@ -14,3 +14,30 @@ var comingSoonString = document.getElementsByTagName('li')[1];
 $('document').ready(function() {
     $(comingSoonString).after(comingSoonEl);
 });
+/** Getting the website selection*/
+var siteSelect = document.getElementsByClassName("siteEl")[0];
+var siteSelect2 = document.getElementsByClassName("siteEl")[1];
+function hoverOnSite1() {
+  $(siteSelect).animate({
+    opacity: 0.5
+  }, "fast");
+}
+function outOnSite1() {
+  $(siteSelect).animate({
+    opacity: 1.0
+  }, "fast");
+}
+function hoverOnSite2() {
+  $(siteSelect2).animate({
+    opacity: 0.5
+  }, "fast");
+}
+function outOnSite2() {
+  $(siteSelect2).animate({
+    opacity: 1.0
+  }, "fast");
+}
+siteSelect.addEventListener("mouseover",hoverOnSite1,false);
+siteSelect.addEventListener("mouseout",outOnSite1,false);
+siteSelect2.addEventListener("mouseover",hoverOnSite2,false);
+siteSelect2.addEventListener("mouseout",outOnSite2,false);
