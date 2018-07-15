71aa1e31da5c4e1bd20eeb6acc797a85e21a36db
Christopher Leveston <36178276+LeeTheTweak@users.noreply.github.com>
2018-07-14 20:17:29 -0500

    Fixed the opacity bug.
    Deleted the extra info in order to fix the opacity mouse over functions.

--- theSite.js
+++ theSite.js
@@ -3,17 +3,6 @@ var chicagoBubbleImage = document.getElementById('bubblesImage');
 $('document').ready(function() {
     $(chicagoBubbleImage).hide().fadeIn(2500);
 });
-
-/**Creating the coming soon element and object
-var comingSoonEl = document.createElement('SPAN');
-var comingSoonTextEl = document.createTextNode('(New)');*/
-
-comingSoonEl.appendChild(comingSoonTextEl);
-
-var comingSoonString = document.getElementsByTagName('li')[1];
-$('document').ready(function() {
-    $(comingSoonString).after(comingSoonEl);
-});
 /** Getting the website selection*/
 var siteSelect = document.getElementsByClassName("siteEl")[0];
 var siteSelect2 = document.getElementsByClassName("siteEl")[1];
